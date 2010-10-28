b6749289a4be91f11dd5804fa1ccc7a2ad08f440
wally <wallyjoe8413@gmail.com>
2010-10-27 23:20:15 -0400

    add asd

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
   :port                 => 587,
   :domain               => "sample.com",
   :user_name            => "wallyjoe8413",
-  :password             => "secret",
+  :password             => "lastaccount",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
